a1c414bde891801a632c1eab1e9ebff3ae649598
arioprima <arioprima2@gmail.com>
2023-12-27 23:14:43 +0700

    update version to 1.0.3

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
 
   const [loading, isLoading] = useState(false);
 
-  const version = "1.0.2";
+  const version = "1.0.3";
 
   const isAuthenticated = () => {
     return localStorage.getItem("JwtToken") !== null;
