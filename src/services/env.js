1d251ac665d5fff63fea07c5e7d204de2bbfdaa8
Ario Prima <99822382+arioprima@users.noreply.github.com>
2024-01-03 07:39:08 +0700

    change base_url

--- src/services/env.js
+++ src/services/env.js
@@ -1,4 +1,4 @@
-export const url_dev = "http://10.20.68.82/molina-lte/api/";
+export const url_dev = "http://10.20.66.97/molina-lte/api/";
 
 export const url_pg_sendbox =
   "https://sandbox.plink.co.id/PrismaGateway/events/voa/v1.0/payment";
