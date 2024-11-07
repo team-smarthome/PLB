68cff3737406c25416495b5c0912fbf4d8c8998a
arioprima <arioprima2@gmail.com>
2024-11-07 18:37:41 +0700

    update socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,12 +6,14 @@ let socket;
 let socket4010;
 
 let socket4020;
+let socket4040;
 
 let pendingTakePhotoRequests = [];
 
 let pendingTakePhotoRequests4010 = [];
 
 let pendingTakePhotoRequests4020 = [];
+let pendingGetDocumentRequests = [];
 
 export const initiateSocket = () => {
     if (!socket) {
@@ -128,6 +130,7 @@ export const initiateSocket4020 = () => {
                 while (pendingTakePhotoRequests4020.length > 0) {
                     const { action, data } = pendingTakePhotoRequests4020.shift();
                     if (action === 'logHistory2') {
+                        console.log('testkesini')
                         socket4020.emit("logHistory2", data);
                     } else {
                         socket4020.emit("logHistory");
@@ -170,4 +173,42 @@ export const addPendingRequest4020 = (request) => {
 
 };
 
+export const initiateSocket4040 = () => {
+    console.log('testWebsocket4040')
+    if (!socket4040) {
+        socket4040 = io('http://localhost:4040');
+        console.log('testWebsocket40402');
+
+        // Event handler untuk koneksi berhasil
+        socket4040.on('connect', () => {
+            console.log('testWebsocket Socket connection established');
+            while (pendingGetDocumentRequests.length > 0) {
+                const { action, data } = pendingGetDocumentRequests.shift();
+                socket.emit("get-documnent");
+            }
+        });
+
+        // Event handler untuk koneksi terputus
+        socket4040.on('disconnect', () => {
+            console.log('testWebsocket Socket disconnected. Attempting to reconnect...');
+        });
+
+        // Event handler untuk reconnect attempt
+        socket4040.on('reconnect_attempt', (attemptNumber) => {
+            console.log(`testWebsocket Reconnect attempt ${attemptNumber}`);
+        });
+
+        // Event handler untuk reconnect berhasil
+        socket4040.on('reconnect', () => {
+            console.log('testWebsocket Reconnected to socket server');
+        });
+
+        // Event handler untuk reconnect gagal
+        socket4040.on('reconnect_failed', () => {
+            console.log('testWebsocket Reconnect failed');
+        });
+    }
+    console.log('testWebsocket40403')
+    return socket4040;
+};
 
